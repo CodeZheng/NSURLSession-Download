bfab656d9a8f9a9ba4c232947c37b5d8494082f5
codezheng <codezheng@163.com>
2016-11-21 11:12:55 +0800

    create project

---
+++ NSURLSession-Download/NSURLSession-Download/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLSession-Download
+//
+//  Created by admin on 21/11/2016.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
